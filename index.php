f77ec24bd43199c18db044e8dcb44786852d16af
Diamond Philip <52967896+Diamond101@users.noreply.github.com>
2021-06-11 13:06:16 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,30 +7,29 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What  \n";
-    $response .= "1. My A \n";
-    $response .= "2. My p";
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Choose a view \n";
-    $response .= "1. Acc \n";
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your number is ".$phoneNumber;
+    $response = "END Your phone number is ".$phoneNumber;
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
     $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your acc is ".$accountNumber;
+    $response = "END Your account number is ".$accountNumber;
 
 }
+
 // Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
-
-?>
